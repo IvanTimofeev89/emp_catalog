dedb44c183d1ff708251d70f35e757cfb9c83058
Ivan Timofeev <timofeevis89@gmail.com>
2024-04-24 17:01:14 +0300

    dockerfile changed

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 python employees_catalog_pj/manage.py makemigrations
 python employees_catalog_pj/manage.py migrate
 python employees_catalog_pj/manage.py db_population --levels=5 --emp_amount=50
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin>
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin')" | python manage.py shell
 python employees_catalog_pj/manage.py runserver 0.0.0.0:8000
